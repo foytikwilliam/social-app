951f459d7d448e468137d82a26a842ade52c22be
Aragon <wfoytik23@gmail.com>
2024-01-24 21:59:54 +0000

    add gym-reviews

--- data/user.go
+++ data/user.go
@@ -90,3 +90,10 @@ func (session *Session) DeleteByUUID() (err error) {
 	_, err = stmt.Exec(session.Uuid)
 	return
 }
+
+func (session *Session) User() (user User, err error) {
+	user = User{}
+	err = Db.QueryRow("SELECT id, uuid, name, email, created_at FROM users WHERE id = $1", session.UserId).
+		Scan(&user.Id, &user.Uuid, &user.Name, &user.Email, &user.CreatedAt)
+	return
+}
